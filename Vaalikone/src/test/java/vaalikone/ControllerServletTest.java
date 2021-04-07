4f2232a0b1b220ddfbdb626b3425fd5a4bcdeb6a
Eemi Karhu <eemi19100@student.hamk.fi>
2021-04-07 21:21:50 +0300

    Corrected tests

--- Vaalikone/src/test/java/vaalikone/ControllerServletTest.java
+++ Vaalikone/src/test/java/vaalikone/ControllerServletTest.java
@@ -2,15 +2,17 @@ package vaalikone;
 
 import java.io.IOException;
 
+import javax.servlet.ServletException;
+
 import org.junit.Assert;
 import org.junit.Test;
 
-public class HelloAppEngineTest {
+public class ControllerServletTest {
 
   @Test
-  public void test() throws IOException {
+  public void test() throws IOException, ServletException {
     MockHttpServletResponse response = new MockHttpServletResponse();
-    new HelloAppEngine().doGet(null, response);
+    new ControllerServlet().doGet(null, response);
     Assert.assertEquals("text/plain", response.getContentType());
     Assert.assertEquals("UTF-8", response.getCharacterEncoding());
     Assert.assertEquals("Hello App Engine!\r\n", response.getWriterContent().toString());
