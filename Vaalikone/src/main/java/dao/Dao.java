120e24e7701fe4ac14138fa75e273e0ed2a75a87
Tomi Lehto <tomi.lehto@gmail.com>
2021-04-08 11:50:32 +0300

    Väittämien deletointi lisätty

--- Vaalikone/src/main/java/dao/Dao.java
+++ Vaalikone/src/main/java/dao/Dao.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 import conn.Connections;
+import data.Fish;
 import data.Vaittama;
 
 //Yhteyden luominen
@@ -70,51 +71,55 @@ public class Dao {
 
 	}
 
-	// PÄIVITETÄÄN JOTAIN KALAA TIETOKANNASSA
-	public ArrayList<Vaittama> updateVaittama(Vaittama f) {
+	// PÄIVITETÄÄN TIETTY VÄITTÄMÄ TIETOKANNASSA
+	public ArrayList<Vaittama> updateVaittama(Vaittama vaittama) {
 		try {
-			// TÄSSÄ PITÄÄ KÄYTTÄÄ PREPARESTATEMENTTIA KOSKA LAUSE SISÄLTÄÄ SYÖTETTYJÄ
-			// MUUTTUJIA.
-			// ELI PELKKÄ STATEMENT EI RIITTÄÄ
+
 			String sql = "update vaittamat set teksti=? where id=?";
 			PreparedStatement pstmt = conn.prepareStatement(sql);
-			// STRING SGL LAUSEEN ENSIMMÄISEN (1) KYSYMYSMERKIN KOHDALLE (?) ASETETAAN ID
-			// ELI TÄLLE METODILLE PARAMETRINA SYÖTETTY BREED
-			pstmt.setString(1, f.getTeksti());
-			// STRING SGL LAUSEEN TOISEN (2) KYSYMYSMERKIN KOHDALLE (?) ASETETAAN ID
-			// ELI TÄLLE METODILLE PARAMETRINA SYÖTETTY ID
-			pstmt.setInt(2, f.getId());
-			// PÄIVITETÄÄN TIETOKANTA (EXECUTEQUERY LUKIESSA, MUUTEN EXECUTEUPDATE)
+
+			pstmt.setString(1, vaittama.getTeksti());
+			pstmt.setInt(2, vaittama.getId());
 			pstmt.executeUpdate();
-			// PALAUTETAAN readAllFish() METODI, JOKA KÄY MUUTTUNEEN LISTAN UUDELLEEN LÄPI
+
 			return listVaittama();
 		} catch (SQLException e) {
 			return null;
 		}
 	}
 	
+	// LUETAAN TIETTY VÄITTÄMÄ TIETOKANNASTA
 	public Vaittama readVaittama(String id) {
 		Vaittama vaittama=null;
 		try {
-			// HAETAAN TAULUKOSTA KALA, JONKA ID VASTAA METODILLE PARAMETRINA SYÖTETTYÄ ID:TÄ
 			String sql="select * from vaittamat where id=?";
-			// TÄSSÄ PITÄÄ KÄYTTÄÄ PREPARESTATEMENTTIA KOSKA LAUSE SISÄLTÄÄ SYÖTETTYJÄ MUUTTUJIA. 
-			// ELI PELKKÄ STATEMENT EI RIITTÄÄ
+
 			PreparedStatement pstmt=conn.prepareStatement(sql);
-			// STRING SGL LAUSEEN ENSIMMÄISEN (1) KYSYMYSMERKIN KOHDALLE (?) ASETETAAN ID 
-			// ELI TÄLLE METODILLE PARAMETRINA SYÖTETTY ID
+
 			pstmt.setString(1, id);
-			// LUETAAN TIETOKANNASTA (EXECUTEQUERY LUKIESSA, MUUTEN EXECUTEUPDATE)
+
 			ResultSet RS=pstmt.executeQuery();
 			while (RS.next()){
-				// LUODAAN UUSI KALA OLIO
+
 				vaittama = new Vaittama();
-				// KALALLE ANNETAAN ROTU TIETOKANNAN BREED SARAKKEEN MUKAAN
 				vaittama.setID(RS.getInt("id"));
 				vaittama.setTeksti(RS.getString("teksti"));
 			}
-			// PALAUTETAAN KALA OLIO F
 			return vaittama;
+		}
+		catch(SQLException e) {
+			return null;
+		}
+	}
+	
+	// POISTETAAN VÄITTÄMÄ TIETOKANNASTA
+	public ArrayList<Vaittama> deleteVaittama(String id) {
+		try {
+			String sql="delete from vaittamat where id=?";
+			PreparedStatement pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.executeUpdate();
+			return listVaittama();
 		}
 		catch(SQLException e) {
 			return null;
