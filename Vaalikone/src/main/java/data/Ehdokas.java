526401c551c6590015700a5b512d94f79c00de29
Eemi Karhu <eemi19100@student.hamk.fi>
2021-05-15 22:32:42 +0300

    Seriazble

--- Vaalikone/src/main/java/data/Ehdokas.java
+++ Vaalikone/src/main/java/data/Ehdokas.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import java.io.Serializable;
 
 @Entity
-public class Ehdokas {
+public class Ehdokas implements Serializable{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)	
